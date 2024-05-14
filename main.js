7849207c8a69dd893b5022cc01c07a5524e0b58c
Barni <if23b200@technikum-wien.at>
2024-05-14 12:42:16 +0200

    getSongs is now working

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const express = require('express');
 const session = require('express-session');
 const querystring = require('querystring');
 const request = require('request');
+
 const app = express();
 const port = 5000;
 
 const CLIENT_ID = 'ac741c3507aa4edc81755b589eef91e4';
 const CLIENT_SECRET = 'a27b89cc9ecc476ba75a6d939f0f115c';
-const REDIRECT_URI = 'http://localhost:5000/callback';
+const REDIRECT_URI = 'http://localhost:' + port + '/callback';
 
 const AUTH_URL = 'https://accounts.spotify.com/authorize';
 const TOKEN_URL = 'https://accounts.spotify.com/api/token';
 const API_BASE_URL = 'https://api.spotify.com/v1/';
-
 
 app.use(session({
     secret: CLIENT_SECRET,
@@ -47,29 +47,99 @@ app.get('/callback', (req, res) => {
             code: req.query.code,
             grant_type: 'authorization_code',
             redirect_uri: REDIRECT_URI,
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
+            //client_id: CLIENT_ID,
+            //client_secret: CLIENT_SECRET,
         };
 
-        request.post({ url: TOKEN_URL, form: reqBody }, (error, response, body) => {
+        const reqHeader = {
+            'content-type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+        }
+
+        request.post({ url: TOKEN_URL, form: reqBody, headers: reqHeader }, (error, response, body) => {
             const tokenInfo = JSON.parse(body);
-            req.session.access_token = tokenInfo.access_token;
-            req.session.refresh_token = tokenInfo.refresh_token;
+            console.log("Token info: ", tokenInfo);
+            session.access_token = tokenInfo.access_token;
+            session.refresh_token = tokenInfo.refresh_token;
 
             const tokenExpiresInSeconds = tokenInfo.expires_in;
             const currentTimeMilliseconds = new Date().getTime();
             const expirationTimeMilliseconds = currentTimeMilliseconds + (tokenExpiresInSeconds * 1000);
             
-            req.session.expires_at = expirationTimeMilliseconds;
+            session.expires_at = expirationTimeMilliseconds;
             res.redirect('/logged');
         });
+
+        /*var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+              code: req.query.code,
+              redirect_uri: REDIRECT_URI,
+              grant_type: 'authorization_code'
+            },
+            headers: {
+              'content-type': 'application/x-www-form-urlencoded',
+              Authorization: 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+            },
+            json: true
+          };
+      
+        request.post(authOptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+    
+            var access_token = body.access_token,
+                refresh_token = body.refresh_token;
+    
+            var options = {
+                url: 'https://api.spotify.com/v1/me',
+                headers: { 'Authorization': 'Bearer ' + access_token },
+                json: true
+            };
+    
+            // use the access token to access the Spotify Web API
+            request.get(options, function(error, response, body) {
+                console.log(body);
+            });
+    
+            // we can also pass the token to the browser to make requests from there
+            res.redirect('/#' +
+            querystring.stringify({
+                access_token: access_token,
+                refresh_token: refresh_token
+            }));
+        } else {
+            res.redirect('/#' +
+            querystring.stringify({
+                error: 'invalid_token'
+            }));
+        }
+        });*/
     }
 });
 
 app.get('/logged', (req, res) => {
-    res.sendFile(__dirname + "/views/logged.html")
-})
+    res.sendFile(__dirname + "/views/logged.html");
+});
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 });
+
+app.get('/getSongs', (req, res) => {
+    var type = "artists";
+    var apiUrl = API_BASE_URL + "me/top/" + type;
+
+    request.get({
+        url: apiUrl,
+        headers: {
+            'Authorization': 'Bearer ' + session.access_token
+        }
+    }, (error, response, body) => {
+        if (error) {
+            res.json({ error });
+        } else {
+            res.json(JSON.parse(body));
+        }
+    })
+});
+
---
+++ public/js/display.js
@@ -0,0 +1,7 @@
+const CLIENT_ID = 'ac741c3507aa4edc81755b589eef91e4';
+const CLIENT_SECRET = 'a27b89cc9ecc476ba75a6d939f0f115c';
+const REDIRECT_URI = 'http://localhost:5000/callback';
+
+
+
+
